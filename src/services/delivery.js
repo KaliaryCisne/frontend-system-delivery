670cd8237c5b057c98538b63ace04a798b6f527a
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2021-04-21 23:05:22 -0300

    Correção de bugs

--- src/services/delivery.js
+++ src/services/delivery.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://backend-system-delivery.herokuapp.com/api/v1/delivery',
+    baseURL: 'https://backend-system-delivery.herokuapp.com/api/v1/delivery',
 });
 
 
